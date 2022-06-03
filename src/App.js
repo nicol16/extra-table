1fdc742e97b10eb0fbe957455e10eab48d5a774b
nicol <nsanchezuceta@gmail.com>
2022-06-03 11:52:10 -0400

    extra-table

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import TableComponent from './table/TableComponent';
+import TableComponent2 from "./table/TableComponent2";
+
+
+function App() {
+  return (
+    <div className="App">
+
+        {/*<TableComponent/>*/}
+        <TableComponent2/>
+
+    </div>
+  );
+}
+
+export default App;
