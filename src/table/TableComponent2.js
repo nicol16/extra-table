c674239d325345a9f866de3dbcf772eee53fdf2d
nicol <nsanchezuceta@gmail.com>
2022-06-03 16:11:28 -0400

    other extra-table finished.

--- src/table/TableComponent2.js
+++ src/table/TableComponent2.js
@@ -7,59 +7,65 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+    { id: 'name', label: 'Name', minWidth: 100 },
+
     {
-        id: 'population',
-        label: 'Population',
+        id: 'genre',
+        label: 'Genre',
         minWidth: 170,
-        align: 'right',
+        align: 'justify',
         format: (value) => value.toLocaleString('en-US'),
     },
     {
-        id: 'size',
-        label: 'Size\u00a0(km\u00b2)',
+        id: 'developer',
+        label: 'Developer',
         minWidth: 170,
-        align: 'right',
+        align: 'justify',
         format: (value) => value.toLocaleString('en-US'),
     },
     {
-        id: 'density',
-        label: 'Density',
+        id: 'gameModes',
+        label: 'Game Modes',
         minWidth: 170,
-        align: 'right',
+        align: 'justify',
         format: (value) => value.toFixed(2),
     },
 ];
 
-function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
-}
-
-const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-];
+// function createData(name, code, population, size) {
+//     const density = population / size;
+//     return { name, code, population, size, density };
+// }
+//
+// const rows = [
+//     createData('India', 'IN', 1324171354, 3287263),
+//     createData('China', 'CN', 1403500365, 9596961),
+//     createData('Italy', 'IT', 60483973, 301340),
+//     createData('United States', 'US', 327167434, 9833520),
+//     createData('Canada', 'CA', 37602103, 9984670),
+//     createData('Australia', 'AU', 25475400, 7692024),
+//     createData('Germany', 'DE', 83019200, 357578),
+//     createData('Ireland', 'IE', 4857000, 70273),
+//     createData('Mexico', 'MX', 126577691, 1972550),
+//     createData('Japan', 'JP', 126317000, 377973),
+//     createData('France', 'FR', 67022000, 640679),
+//     createData('United Kingdom', 'GB', 67545757, 242495),
+//     createData('Russia', 'RU', 146793744, 17098246),
+//     createData('Nigeria', 'NG', 200962417, 923768),
+//     createData('Brazil', 'BR', 210147125, 8515767),
+// ];
 
 export default function TableComponent2() {
+
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+
+    const [datos, setDatos] = useState([]);
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -70,7 +76,29 @@ export default function TableComponent2() {
         setPage(0);
     };
 
+
+    function obtenerDatos() {
+       axios.get(`http://10.0.0.128:88/api/v1/video-games`).then(response => {
+            setDatos(response.data);
+
+        });
+    }
+
+    useEffect(() => {
+
+        obtenerDatos();
+
+
+    }, []);
+
+
+
+
+
+
     return (
+
+        <>
         <Paper sx={{ width: '100%', overflow: 'hidden' }}>
             <TableContainer sx={{ maxHeight: 440 }}>
                 <Table stickyHeader aria-label="sticky table">
@@ -88,21 +116,15 @@ export default function TableComponent2() {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows
+                        {datos
                             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row) => {
+                            .map((dato) => {
                                 return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                        {columns.map((column) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <TableCell key={column.id} align={column.align}>
-                                                    {column.format && typeof value === 'number'
-                                                        ? column.format(value)
-                                                        : value}
-                                                </TableCell>
-                                            );
-                                        })}
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={columns.id}>
+                                        <TableCell>{dato.name}</TableCell>
+                                        <TableCell>{dato.genre}</TableCell>
+                                        <TableCell>{dato.developer}</TableCell>
+                                        <TableCell>{dato.gameModes}</TableCell>
                                     </TableRow>
                                 );
                             })}
@@ -112,12 +134,13 @@ export default function TableComponent2() {
             <TablePagination
                 rowsPerPageOptions={[5, 10, 25, 100]}
                 component="div"
-                count={rows.length}
+                count={datos.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 onPageChange={handleChangePage}
                 onRowsPerPageChange={handleChangeRowsPerPage}
             />
         </Paper>
+        </>
     );
 }
